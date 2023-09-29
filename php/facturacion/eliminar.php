2ffbe917ec3239c1c5f712a6d5022fa50de37b43
CLINICARE <edwin.velasquez@clinicarehn.com>
2023-09-28 19:12:29 -0600

    Update eliminar.php

--- php/facturacion/eliminar.php
+++ php/facturacion/eliminar.php
@@ -11,18 +11,18 @@ $usuario = $_SESSION['colaborador_id'];
 //CONSULTAMOS EL NUMERO DE LA MUESTRA
 $query_muestra = "SELECT muestras_id
 	FROM facturas
-	WHERE facturas_id = '$facturas_id' AND estado = 1";
+	WHERE facturas_id = '$facturas_id'";
 $result_muestras = $mysqli->query($query_muestra) or die($mysqli->error);
 
-//ELIMINAMOS EL DETALLE DE LA FACTURA
-$delete_detalle = "DELETE FROM facturas_detalle WHERE facturas_id = '$facturas_id'";
-$mysqli->query($delete_detalle);
-
 //ELIIMINAMOS LA FACTURA
-$delete_factura = "DELETE FROM facturas WHERE facturas_id = '$facturas_id'";
+$delete_factura = "DELETE FROM facturas WHERE facturas_id = '$facturas_id' AND estado = 1";
 $query = $mysqli->query($delete_factura);
 
 if($query){
+	//ELIMINAMOS EL DETALLE DE LA FACTURA
+	$delete_detalle = "DELETE FROM facturas_detalle WHERE facturas_id = '$facturas_id'";
+	$mysqli->query($delete_detalle);
+
 	echo 1;//REGISTRO ELIMINADO CORRECTAMENTE
 
 	if($result_muestras->num_rows>0){
