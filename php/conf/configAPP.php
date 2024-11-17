6f4b771e05b1d55f17aa8121f4db098d89bb22b4
Edwin Velasquez <105985102+clinicarehn@users.noreply.github.com>
2024-11-16 21:37:04 -0600

    Update configAPP.php

--- php/conf/configAPP.php
+++ php/conf/configAPP.php
@@ -1,30 +1,31 @@
 <?php
-		/*
-		  Parametros de conexión a la DB
-		*/
 
-		//DATOS DE CONEXION DEL CLIENTE
-		date_default_timezone_set('America/Tegucigalpa');
-		const SERVERURL = "http://localhost/devpatolab/";
-		const SERVEREMPRESA = "Patolab";
-		const SERVER = "localhost";
-		const DB = "clinicarehn_clientes_patolab";
-		const USER = "clinicarehn_clinicare";
-		const PASS = "Clin1c@r32022#";
+/*
+ * Parametros de conexión a la DB
+ */
 
-		const PRODUCT_PATH = "/devpatolab/php/atencion_pacientes/Examenes/";
+// DATOS DE CONEXION DEL CLIENTE
+date_default_timezone_set('America/Tegucigalpa');
 
-		//DATOS DE CONEXION SERVIDOR PRINCIPAL
-		const SERVER_MAIN = "localhost";
-		const DB_MAIN = "clinicarehn_fayad_clinicare";
-		const USER_MAIN = "clinicarehn_clinicare";
-		const PASS_MAIN = "Clin1c@r32022#%.";
+const SERVERURL = 'http://localhost/devpatolab/';
+const SERVEREMPRESA = 'Patolab';
+const SERVER = 'localhost';
+const DB = 'clinicarehn_patolab';
+const USER = 'clinicarehn_clinicare';
+const PASS = 'Clin1c@r32022#';
+const PRODUCT_PATH = '/devpatolab/php/atencion_pacientes/Examenes/';
 
-		/*
-		  Para encrptar y Desencriptar
-		  Nota: Estos valores no se deben cambiar, si hay datos en la DB
-		*/
-		const METHOD = "AES-256-CBC";
-		const SECRET_KEY = '$DP_@2020';
-		const SECRET_IV = '10172';
-		const SISTEMA_PRUEBA = "SI"; //SI o NO
+// DATOS DE CONEXION SERVIDOR PRINCIPAL
+const SERVER_MAIN = 'localhost';
+const DB_MAIN = 'clinicarehn_clinicare';
+const USER_MAIN = 'clinicarehn_clinicare';
+const PASS_MAIN = 'Clin1c@r32022#%.';
+
+/*
+ * Para encrptar y Desencriptar
+ * Nota: Estos valores no se deben cambiar, si hay datos en la DB
+ */
+const METHOD = 'AES-256-CBC';
+const SECRET_KEY = '$DP_@2020';
+const SECRET_IV = '10172';
+const SISTEMA_PRUEBA = 'SI';  // SI o NO
